5941c828c74a1c4a15f5c9754848dfff31ac29e6
Emiliano <emiliano.cabello7@iems.edu.mx>
2024-02-05 16:16:42 -0600

    fix: :fire: I've delete the function display screen
    The DisplayScreen's and showMeTheNum's functions have been merged in one function

--- calculator.js
+++ calculator.js
@@ -1,23 +1,24 @@
-let digit1;
-let digit2;
+let digit1 = 0;
+let digit2 = 0;
 let operator;
 
-function add (num1, num2) {
-    const sum = num1 +  num2;
+
+function add(num1, num2) {
+    const sum = num1 + num2;
     return sum
 }
 
-function subtract (num1, num2) {
+function subtract(num1, num2) {
     const remainder = num1 - num2;
     return remainder
 }
 
-function multiply (num1, num2) {
+function multiply(num1, num2) {
     const increase = num1 * num2;
-    return increase 
+    return increase
 }
 
-function beDivide (num1, num2) {
+function beDivide(num1, num2) {
     const divide = num1 / num2;
     return divide
 }
@@ -26,15 +27,34 @@ function operate(operation, num1, num2) {
     const result = operation(num1, num2);
     return result
 }
-
 const displayValue = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 const screen = document.querySelector('.screen');
+let currentNum = '';
 
-function DisplayScreen(digit) {
-    screen.innerHTML = `<span class='digit-style'>${digit}</span>`;
-}
 
-displayValue.forEach((digits) => {
-    digits.addEventListener('click',(e) => DisplayScreen(e.target.textContent))
-});
+
+
+// function clickOperator() {
+//     operators.forEach(typeOp => {
+//         typeOp.addEventListener('click', (e) => {
+//             currentOperator = e.target.textContent;
+//         })
+//     })
+// }
+
+// clickOperator();
+
+function showMeTheNum() {
+    displayValue.forEach((digits) => {
+        digits.addEventListener('click', (e) => {
+            currentNum += e.target.textContent;
+            numInt = currentNum;
+            screen.innerHTML = `<span class='digit-style'>${numInt}</span>`
+        })
+    })
+} 
+
+showMeTheNum();
+
+//The display screen's and showMeTheNum's functions have been merged in one function 
